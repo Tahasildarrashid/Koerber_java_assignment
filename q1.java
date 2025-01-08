43b599f9ea15d499d92318ebed18954408e38cd0
Tahasildarrashid <tahasildarrashid233@gmail.com>
2025-01-08 10:17:53 +0530

    testing

---
+++ q1.java
@@ -0,0 +1,15 @@
+class Employee
+{
+    private int id;
+    private String firstname;
+    private String lastname;
+    private int Salary;
+}
+
+public class q1 {
+    public static void main(String[] args)
+    {
+
+    }
+}
+
