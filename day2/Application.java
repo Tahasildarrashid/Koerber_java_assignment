d9e605c23101bf0da50f840232ff5fbf4d4f9842
Rashid T Tahasildar <85626896+Tahasildarrashid@users.noreply.github.com>
2025-01-09 14:56:23 +0530

    Update Application.java
    Confirmed

--- day2/Application.java
+++ day2/Application.java
@@ -5,9 +5,10 @@ public class Application {
     {
         Employee e = new Employee(1, "rashid t", 12345);
         System.out.println(e.getPayment());
-
+        e.toString();
         Invoice inv = new Invoice("Office chairs", "Workhub by Novel Office", 100, 1500);
         System.out.println(inv.getPayment());
+        inv.toString();
     }
 
 
