5b2dcac03951d2898121f9cd261cb8f89ca015e5
Rashid T Tahasildar <85626896+Tahasildarrashid@users.noreply.github.com>
2025-01-09 14:48:47 +0530

    Add files via upload

---
+++ day2/Application.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class Application {
+    public static void main(String[] args)
+    {
+        Employee e = new Employee(1, "rashid t", 12345);
+        System.out.println(e.getPayment());
+
+        Invoice inv = new Invoice("Office chairs", "Workhub by Novel Office", 100, 1500);
+        System.out.println(inv.getPayment());
+    }
+
+
+}
